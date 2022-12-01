3bcfc16873a5469e264eba76d927c27758014c50
Yonattan <yonattan_hope@yahoo.com>
2022-12-01 14:09:38 +0700

    pos with table selection

--- lib/module/exercise/ls_pos_with_table_selection/controller/ls_pos_with_table_selection_controller.dart
+++ lib/module/exercise/ls_pos_with_table_selection/controller/ls_pos_with_table_selection_controller.dart
@@ -1,8 +1,6 @@
 import 'package:faker_dart/faker_dart.dart';
 import 'package:flutter/material.dart';
-import 'package:example/state_util.dart';
-import '../../../../shared/util/dialog/show_info_dialog.dart';
-import '../view/ls_pos_with_table_selection_view.dart';
+import '../../../../core.dart';
 
 class LsPosWithTableSelectionController
     extends State<LsPosWithTableSelectionView> implements MvcController {
@@ -41,6 +39,8 @@ class LsPosWithTableSelectionController
     Jika belum tambahkan di ProductCRUD (tasks sebelumnya)
     Cukup tambahkan 5 saja
     */
+    productList = mainStorage.get("products") ?? [];
+    setState(() {});
   }
 
   increaseQty(item) {
@@ -52,6 +52,8 @@ class LsPosWithTableSelectionController
     setState(() {});
     ###
     */
+    item["qty"]++;
+    setState(() {});
   }
 
   decreaseQty(item) {
@@ -64,6 +66,9 @@ class LsPosWithTableSelectionController
     setState(() {});
     ###
     */
+    if (item["qty"] == 0) return;
+    item["qty"]--;
+    setState(() {});
   }
 
   double get total {
@@ -78,6 +83,10 @@ class LsPosWithTableSelectionController
     }
     ###
     */
+    for (var i = 0; i < productList.length; i++) {
+      var product = productList[i];
+      itemTotal += product["qty"] * product["price"];
+    }
     return itemTotal;
   }
 
